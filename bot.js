deaa2293439de4c36e7bd7c721c82558304a612c
dakgogi <jeffreyshu@me.com>
2017-04-30 18:44:56 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,8 @@ function respond() {
       botRegexSC = new RegExp("SC"); 
       botSC1 = new RegExp("starcraft"); botSC2 = new RegExp("StarCraft"); 
       botRegexMC = new RegExp("MC"); 
+      botRegexHW = new RegExp("hammerwatch"); 
+      botHW1 = new RegExp("Hammerwatch");
       botRegexClayton = /^\/Clayton/; 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -72,6 +74,11 @@ function respond() {
       postMessage("DIE ALREADY");
     this.res.end();
    }
+   else if(request.text && botRegexHW.test(request.text)) { 
+    this.res.writeHead(200);
+    postMessage("level 3 armor store kreygasm");
+    this.res.end();
+  } 
   else {
     console.log("Nani the fuck did you just fucking iimasu about watashi, you chiisai bitch desuka? Watashi’ll have anata know that watashi graduated top of my class in Nihongo 3, and watashi’ve been involved in iroirona Nihongo tutoring sessions, and watashi have over sanbyaku perfect test scores. Watashi am trained in kanji, and watashi is the top letter writer in all of southern California. Anata are nothing to watashi but just another weaboo. Watashi will korosu anata the fuck out with vocabulary the likes of which has never been mimasu’d before on this continent, mark watashino fucking words. Anata thinks anata can get away with hanashimasing that kuso to watashi over the intaaneto? Omou again, fucker. As we hanashimasu, watashi am contacting watashino secret netto of otakus across the USA, and anatano IP is being traced right now so you better junbishimasu for the ame, ujimushi. The ame that korosu’s the pathetic chiisai thing anata calls anatano life. You’re fucking shinimashita’d, akachan.");
     this.res.writeHead(200);
