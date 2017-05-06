2f814cb3894e47b0bc4304c37d59ff223388e2bb
dakgogi <jeffreyshu@me.com>
2017-05-06 18:22:49 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -130,6 +130,9 @@ function postMessage(response) {
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
-
-
+/*function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+} */
 exports.respond = respond;
