4dde6fa042366cb46eeb5a219ed3559be2ed3e51
dakgogi <jeffreyshu@me.com>
2017-07-02 21:46:20 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -86,7 +86,7 @@ function respond() {
     this.res.end();
   } 
   else {
-    console.log("Nani the fuck did you just fucking iimasu about watashi, you chiisai bitch desuka? Watashi’ll have anata know that watashi graduated top of my class in Nihongo 3, and watashi’ve been involved in iroirona Nihongo tutoring sessions, and watashi have over sanbyaku perfect test scores. Watashi am trained in kanji, and watashi is the top letter writer in all of southern California. Anata are nothing to watashi but just another weaboo. Watashi will korosu anata the fuck out with vocabulary the likes of which has never been mimasu’d before on this continent, mark watashino fucking words. Anata thinks anata can get away with hanashimasing that kuso to watashi over the intaaneto? Omou again, fucker. As we hanashimasu, watashi am contacting watashino secret netto of otakus across the USA, and anatano IP is being traced right now so you better junbishimasu for the ame, ujimushi. The ame that korosu’s the pathetic chiisai thing anata calls anatano life. You’re fucking shinimashita’d, akachan.");
+    console.log("Something broke :(");
     this.res.writeHead(200);
     this.res.end();
   }
@@ -126,13 +126,14 @@ function postMessage(response) {
   });
   botReq.end(JSON.stringify(body));
 }
-
+/*
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
-/*function getRandomIntInclusive(min, max) {
+*/
+function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-} */
+}
 exports.respond = respond;
