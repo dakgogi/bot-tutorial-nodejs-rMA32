3e12d336d410d99fd4e45224d45342a34bae60bf
dakgogi <jeffreyshu@me.com>
2017-07-02 21:54:36 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -132,8 +132,6 @@ function getRandomInt(min, max) {
 }
 */
 function getRandomIntInclusive(min, max) {
-  min = 0;
-  max = 10;
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
