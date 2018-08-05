bbf6227187f3d931347a6fa547542b94db474245
dakgogi <jeffreyshu@me.com>
2018-08-05 14:58:11 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegexAncap = new RegExp("ancap"); 
+      botRegexAncap = new RegExp("ancap|Ancap"); 
       botRegexDildo = new RegExp("dildo");
   
   
