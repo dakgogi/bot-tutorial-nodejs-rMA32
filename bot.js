cba089a4a2849b3e91f8d87f085febacf467f2a9
dakgogi <jeffreyshu@me.com>
2018-08-04 22:50:40 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,9 +24,17 @@ function respond() {
    
   } 
   else if(request.text && botRegexDildo.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("It's for personal use");
-    this.res.end();
+   switch (getRndInteger(0,2)) {
+    case 0:
+        this.res.writeHead(200);
+        postMessage("It's for personal use");
+        this.res.end();
+        break;
+    case 1:
+        this.res.writeHead(200);
+        postMessage("Differentiating your business dildo and your personal dildo is very important");
+        this.res.end();
+        break;
   } 
   else {
     console.log("Nani the fuck did you just fucking iimasu about watashi, you chiisai bitch desuka? Watashi’ll have anata know that watashi graduated top of my class in Nihongo 3, and watashi’ve been involved in iroirona Nihongo tutoring sessions, and watashi have over sanbyaku perfect test scores. Watashi am trained in kanji, and watashi is the top letter writer in all of southern California. Anata are nothing to watashi but just another weaboo. Watashi will korosu anata the fuck out with vocabulary the likes of which has never been mimasu’d before on this continent, mark watashino fucking words. Anata thinks anata can get away with hanashimasing that kuso to watashi over the intaaneto? Omou again, fucker. As we hanashimasu, watashi am contacting watashino secret netto of otakus across the USA, and anatano IP is being traced right now so you better junbishimasu for the ame, ujimushi. The ame that korosu’s the pathetic chiisai thing anata calls anatano life. You’re fucking shinimashita’d, akachan.");
