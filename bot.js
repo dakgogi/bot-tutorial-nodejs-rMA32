2d74c46c77272369917192c5cb38a088c294ce6d
dakgogi <jeffreyshu@me.com>
2018-08-25 12:20:20 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,18 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegexAncap = new RegExp("ancap|Ancap"); 
       botRegexDildo = new RegExp("dildo|Dildo");
+      botRegexStatSheet = new RegExp("stat sheet"|"Stat sheet"|"STAT SHEET"
+                                     
+    if(request.text && botRegexStatSheet.test(request.text)) {
+          switch (getRndInteger(0,1)) {
+    case 0:
+        this.res.writeHead(200);
+        postMessage("DID SOMEBODY SAY STAT SHEET!!!!!?!?!?!?!?!?!?!");
+        postMessage("*FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP**FAP*");
+        this.res.end();
+        break;
+          }     
+                                  
   
   
   if(request.text && botRegexAncap.test(request.text)) {
@@ -38,7 +50,7 @@ function respond() {
   } 
 }
   else {
-    console.log("Nani the fuck did you just fucking iimasu about watashi, you chiisai bitch desuka? Watashi’ll have anata know that watashi graduated top of my class in Nihongo 3, and watashi’ve been involved in iroirona Nihongo tutoring sessions, and watashi have over sanbyaku perfect test scores. Watashi am trained in kanji, and watashi is the top letter writer in all of southern California. Anata are nothing to watashi but just another weaboo. Watashi will korosu anata the fuck out with vocabulary the likes of which has never been mimasu’d before on this continent, mark watashino fucking words. Anata thinks anata can get away with hanashimasing that kuso to watashi over the intaaneto? Omou again, fucker. As we hanashimasu, watashi am contacting watashino secret netto of otakus across the USA, and anatano IP is being traced right now so you better junbishimasu for the ame, ujimushi. The ame that korosu’s the pathetic chiisai thing anata calls anatano life. You’re fucking shinimashita’d, akachan.");
+    console.log("Something broke :(");
     this.res.writeHead(200);
     this.res.end();
   }
